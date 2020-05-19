2ff94216588954f633441d0a6172f04261006117
EC2 Default User <ec2-user@ip-172-31-20-237.us-east-2.compute.internal>
2020-05-19 15:58:08 +0000

    added my lab2.py file.

---
+++ lab2.py
@@ -0,0 +1,47 @@
+"""
+Lab 2
+"""
+#4.1
+my_name= "Tom"
+print(my_name.upper())
+
+#4.2
+my_id=123
+print(my_id)
+
+#4.3
+"""
+123=my_id
+print(my_id)
+does not work because the variable is not on the left
+and the value is not on the right.
+"""
+my_id=your_id=123
+print(my_id)
+print(your_id)
+
+#4.4
+my_id_str="123"
+print(my_id_str)
+
+#4.5
+#print(my_name+my_id)
+#does not work because my_name is a string and my_id is an integer
+
+#4.6
+print(my_name+my_id_str)
+
+#4.7
+print(my_name*3)
+
+#4.8
+print("hello, world. This is my first python string.".split("."))
+
+#4.9
+"""
+message='Tom's id is 123'
+print(message)
+this does not work because of the confliction
+between the single quote and the apostrophe in Tom'set
+to make this work "" would be needed
+"""
---
+++ lecture2.py
@@ -0,0 +1,58 @@
+"""
+Your module description
+"""
+#print("hello world") #my first attempt at a single line comment
+"""
+this is my second py code for my 2nd lecture
+"""
+
+"""
+2nd regional comment
+"""
+#this is my 2nd line comment
+#print("Hello World".lower())
+
+"""
+print(123)
+print(123.)
+print("123")
+"""
+
+#print("  hello   world  ".strip())
+#print("hello, world".split("l"))
+#print("hello" + "world"+".")
+
+#print(2/3)
+#print(2*3)
+#print(2**3)
+#print(1/2)
+
+#my_str='hello world'
+#print(my_str)
+
+my_name= "Tom"
+print(my_name.upper())
+
+my_id=123
+print(my_id)
+"""
+123=my_id
+print(my_id)
+"""
+my_id=your_id=123
+print(my_id)
+print(your_id)
+
+my_id_str="123"
+print(my_id_str)
+
+#print(my_name+my_id)
+print(my_name+my_id_str)
+
+print(my_name*3)
+
+print("hello, world. This is my first python string.".split("."))
+"""
+message='Tom's id is 123'
+print(message)
+"""
